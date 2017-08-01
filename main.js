b69c0ab07a70d72083abfd7f99bb5a4974ebac25
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-01 13:47:38 -0600

    updated solution

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
 $(document).ready(function(){
-    $("body").on("click", ".editText", (function(){
-        var $txtEd = $(this);
 
-        var $input =$('<input/>').val($txtEd.text());
-        $txtEd.replaceWith($input);
+/* Name Section */
 
-        var save = function () {
-            var $p = $('<p .editText />').text($input.val());
-            $input.replaceWith($p);
-        };
-        $input.one('blur', save).focus();
-    }));
+    $('#nameText').on('click', function(){
+        $('#nameText').addClass('hidden');
+        $('#nameInput').removeClass('hidden');
+        $('#nameInput').trigger('focus');
+        $('#nameInput').val($('#nameText').text());
+    });
+
+    $('#nameInput').on('blur', function(){
+        $('#nameText').removeClass('hidden');
+        $('#nameInput').addClass('hidden');
+        $('#nameText').text($('#nameInput').val());
+    });
+
+/* About Section */
+
+    $('#aboutTxt').on('click', function(){
+        $('#aboutTxt').addClass('hidden');
+        $('#aboutMeText').removeClass('hidden');
+        $('#aboutMeText').trigger('focus');
+        $('#aboutMeText').val($('#aboutTxt').text());
+    });
+
+    $('#aboutMeText').on('blur', function(){
+        $('#aboutTxt').removeClass('hidden');
+        $('#aboutMeText').addClass('hidden');
+        $('#aboutTxt').text($('#aboutMeText').val());
+    });
+
 });
+
+/* -=-=-=-= My First Try =-=-=-=-=-=*/
+// This works but has some weird styling issues.
+
+// $("body").on("click", ".editText", (function(){
+//     var $txtEd = $(this);
+//
+//     var $input = $('<input/>').val($txtEd.text());
+//     $txtEd.replaceWith($input);
+//
+//     var save = function () {
+//         var $p = $('<p .editText />').text($input.val());
+//         $input.replaceWith($p);
+//     };
+//     $input.one('blur', save).focus();
+// }));
