4ffd83d85f8c5b2e02b1309595b18fa336f0b7b7
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-07-31 22:58:35 -0600

    first shot at problem

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-    var editText = $('#editText');
-    editText.on('click', function(){
-        var newText =
-    });
+    $("body").on("click", ".editText", (function(){
+        var $txtEd = $(this);
 
+        var $input =$('<input/>').val($txtEd.text());
+        $txtEd.replaceWith($input);
+
+        var save = function () {
+            var $p = $('<p .editText />').text($input.val());
+            $input.replaceWith($p);
+        };
+        $input.one('blur', save).focus();
+    }));
 });
